07af16541b33ccb401bb24423d40ef2b2a042915
18wh1a0257 <18wh1a0257@bvrithyderabad.edu.in>
2021-05-26 23:58:25 +0530

    Removed unnecessary parts in Trainodel

--- yogapp/public/trainModel.js
+++ yogapp/public/trainModel.js
@@ -8,11 +8,7 @@ let targetLabel;
 let poseLabel = "Def"
 
 function setup(){
-  createCanvas(640,480);
-  video = createCapture(VIDEO);
-  video.hide();
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose',gotPoses);
+  
   let options={
     inputs: 34 ,
     outputs: 4,
@@ -22,22 +18,7 @@ function setup(){
   brain = ml5.neuralNetwork(options);
   brain.loadData('basics.json',dataReady);
 }
-function gotPoses(poses){
- 
-  if(poses.length > 0) {
-    pose = poses[0].pose;
-    skeleton = poses[0].skeleton;
-    let input =[];
-    for(let i = 0; i < pose.keypoints.length;i++){
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      input.push(x);
-      input.push(y);
-    }
-  target = [targetLabel]
-  brain.addData(input,target);
-    }
-  }
+
 
 function dataReady(){
     brain.normalizeData();
